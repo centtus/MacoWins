833740ea9f06e3daf479c43abe532c6e700c2158
nicogon <nicogon@gmail.com>
2016-04-09 14:22:02 -0300

    borrado

--- src/test/java/VentaTest.java
+++ src/test/java/VentaTest.java
@@ -13,14 +13,7 @@ public class VentaTest {
 
 	@Test
 	public void test() {
-//		Ganancias ganancia = new Ganancias();
-	//	Productos camisa = new Camisa(true);
-	//	Venta venta = new Venta(camisa,5);
-	//	Camisa camisa2 = (Camisa) venta.getProducto();
-		
-		//Assert.assertEquals(camisa2.precioFinal(),260,0);
-		
-	
+
 		
 		
 		
