8e0cb7e2772049dbc4ae20189e1bc5c231e26a4a
aldo <aldo_soga@hotmail.com>
2016-04-14 17:41:14 -0300

    correccion zapato

--- src/main/java/Zapato.java
+++ src/main/java/Zapato.java
@@ -2,7 +2,9 @@ package main.java;
 
 public class Zapato extends Productos{
 		
-		public Zapato(int talle){
+		public Zapato(Origen origen, int talle,Marca marca){
 			this.precioBase = 400 + (5 * talle);
+			this.marca = marca;
+			this.origen = origen;
 		}
 }
